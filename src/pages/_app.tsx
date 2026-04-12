0f431221b69ed19f0aefebe022751c3a89c154e5
Ahmetustam <hgfnnhbvmj@gmail.com>
2026-04-12 16:55:19 +0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
     <ChakraProvider theme={theme}>
       <QueryClientProvider client={client}>
         <Head>
-          <title>Demo Bot</title>
+          <title>Gora Bot DashBoard</title>
         </Head>
         {getLayout(<Component {...pageProps} />)}
       </QueryClientProvider>
