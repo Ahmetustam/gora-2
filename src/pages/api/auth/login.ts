098469f6dccb9a7f6da8b54be424a01ed125afed
Ahmetustam <hgfnnhbvmj@gmail.com>
2026-04-13 10:58:28 +0300

    Update login.ts

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,19 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { CLIENT_ID } from '@/utils/auth/server';
-import { getAbsoluteUrl } from '@/utils/get-absolute-url';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { locale } = req.query as {
     locale?: string;
   };
 
-  const url = `https://discord.com/oauth2/authorize?client_id=${new URLSearchParams({
+  // URLSearchParams nesnesini oluşturuyoruz
+  const params = new URLSearchParams({
     client_id: CLIENT_ID,
     redirect_uri: 'https://goradashboard.vercel.app/api/auth/callback',
     response_type: 'code',
     scope: 'identify guilds',
-    state: locale ?? '',
-  })}`;
+    state: locale ?? 'en', // State boş gitmesin, varsayılan 'en' olsun
+  });
+
+  // Linki temiz bir şekilde birleştiriyoruz
+  const url = `https://discord.com/oauth2/authorize?${params.toString()}`;
 
   res.redirect(302, url);
 }
